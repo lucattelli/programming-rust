0c413112a59bbf0d8481d9108d3ed972975b59ea
Bruno Lucattelli <lucattelli@gmail.com>
2022-04-21 22:21:28 -0300

    Refactor main function for better legibility

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -5,6 +5,13 @@ use std::env;
 use std::fs::File;
 use std::str::FromStr;
 
+struct Args {
+    filename: String,
+    resolution: String,
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+}
+
 fn get_args_vector() -> Vec<String> {
     env::args().collect()
 }
@@ -20,13 +27,21 @@ fn check_args(args: &Vec<String>) {
     }
 }
 
-fn main() {
+fn get_args() -> Args {
     let args = get_args_vector();
     check_args(&args);
+    Args {
+        filename: args[1].clone(),
+        resolution: args[2].clone(),
+        upper_left: parse_complex(&args[3]).expect("error parsing upper left corner point"),
+        lower_right: parse_complex(&args[4]).expect("error parsing lower right corner point"),
+    }
+}
 
-    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
-    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
-    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+fn main() {
+    let args = get_args();
+
+    let bounds = parse_pair(&args.resolution, 'x').expect("error parsing image dimensions");
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
@@ -41,9 +56,14 @@ fn main() {
                 let top = calc_band_top(rows_per_band, i);
                 let height = calc_band_height(band.len(), bounds.0);
                 let band_bounds = (bounds.0, height);
-                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
-                let band_lower_right =
-                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+                let band_upper_left =
+                    pixel_to_point(bounds, (0, top), args.upper_left, args.lower_right);
+                let band_lower_right = pixel_to_point(
+                    bounds,
+                    (bounds.0, top + height),
+                    args.upper_left,
+                    args.lower_right,
+                );
 
                 spawner.spawn(move |_| {
                     render(band, band_bounds, band_upper_left, band_lower_right);
@@ -53,7 +73,7 @@ fn main() {
         .unwrap();
     }
 
-    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
+    write_image(&args.filename, &pixels, bounds).expect("error writing PNG file");
 }
 
 fn calc_band_top(rows: usize, index: usize) -> usize {
